b75e665f476af644fe89b0f0a4fc6d5a7fa1e33e
wassek <werner@jahia.com>
2015-08-05 15:32:02 +0200

    SODEXO-36: translate some resources

--- src/main/resources/javascript/i18n/jquery.dataTables-pt.js
+++ src/main/resources/javascript/i18n/jquery.dataTables-pt.js
@@ -1,20 +1,20 @@
 $.extend( $.fn.dataTable.defaults.oLanguage, {
     "sEmptyTable":     "No data available in table",
-    "sInfo":           "Showing _START_ to _END_ of _TOTAL_ entries",
-    "sInfoEmpty":      "Showing 0 to 0 of 0 entries",
-    "sInfoFiltered":   "(filtered from _MAX_ total entries)",
+    "sInfo":           "Mostrando de _START_ até _END_ de _TOTAL_ registos",
+    "sInfoEmpty":      "Mostrando de 0 até 0 de 0 registos",
+    "sInfoFiltered":   "(filtrado de _MAX_ registos no total)",
     "sInfoPostFix":    "",
     "sInfoThousands":  ",",
-    "sLengthMenu":     "Show _MENU_ entries",
+    "sLengthMenu":     "Mostrar _MENU_ registos",
     "sLoadingRecords": "Loading...",
-    "sProcessing":     "Processing...",
-    "sSearch":         "Search:",
-    "sZeroRecords":    "No matching records found",
+    "sProcessing":     "A processar...",
+    "sSearch":         "Procurar:",
+    "sZeroRecords":    "Não foram encontrados resultados",
     "oPaginate": {
-        "sFirst":    "First",
-        "sLast":     "Last",
-        "sNext":     "Next",
-        "sPrevious": "Previous"
+        "sFirst":    "Primeiro",
+        "sLast":     "Anterior",
+        "sNext":     "Seguinte",
+        "sPrevious": "Último"
     },
     "oAria": {
         "sSortAscending":  ": activate to sort column ascending",
