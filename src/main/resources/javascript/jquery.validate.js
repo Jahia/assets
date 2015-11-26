c3247f82db2db371d4e9a1cf50c0fc18fc5698e2
bdjiba <bdjiba@jahia.com>
2015-11-26 12:17:51 +0100

    QA-5792: Better to avoid issue in the future with the use of attachEvent + remove a focus in out code fragment

--- src/main/resources/javascript/jquery.validate.js
+++ src/main/resources/javascript/jquery.validate.js
@@ -1120,40 +1120,10 @@ $.format = $.validator.format;
 	}
 })(jQuery);
 
-// provides cross-browser focusin and focusout events
-// IE has native support, in other browsers, use event caputuring (neither bubbles)
-
 // provides delegate(type: String, delegate: Selector, handler: Callback) plugin for easier event delegation
 // handler is only called when $(event.target).is(delegate), in the scope of the jquery-object for event.target
 ;(function($) {
-	// only implement if not provided by jQuery core (since 1.4)
-	// TODO verify if jQuery 1.4's implementation is compatible with older jQuery special-event APIs
-	// JAHAIA: fix issue in IE[9) by excluding window attachEvent regarding https://github.com/jzaefferer/jquery-validation/issues/542
-	if (!jQuery.event.special.focusin && !jQuery.event.special.focusout && !window.attachEvent && document.addEventListener) {
-		$.each({
-			focus: 'focusin',
-			blur: 'focusout'
-		}, function( original, fix ){
-			$.event.special[fix] = {
-				setup:function() {
-					this.addEventListener( original, handler, true );
-				},
-				teardown:function() {
-					this.removeEventListener( original, handler, true );
-				},
-				handler: function(e) {
-					arguments[0] = $.event.fix(e);
-					arguments[0].type = fix;
-					return $.event.handle.apply(this, arguments);
-				}
-			};
-			function handler(e) {
-				e = $.event.fix(e);
-				e.type = fix;
-				return $.event.handle.call(this, e);
-			}
-		});
-	};
+	// JAHAIA QA-5792 apply patch that fix https://github.com/jzaefferer/jquery-validation/issues/542
 	$.extend($.fn, {
 		validateDelegate: function(delegate, type, handler) {
 			return this.bind(type, function(event) {
